8271ac32d478440e618de51931bec0106e29d6b3
Laoma Nogueira <laomanogueira@gmail.com>
2021-08-27 00:11:40 -0300

    organizando arquivos

--- aula 23-08/conversorTemperatura.js
+++ aula 23-08/conversorTemperatura.js
@@ -8,34 +8,3 @@ function converterTemp(temperaturaC) {
 let tempCelsius = rLine.questionFloat(`Informe a temperatura em °C: \n`);
 
 converterTemp(tempCelsius);
-
-
-
-//converte a temperatura, de acordo com a escala de origem e a desejada: 
-function converteTemperatura(temperatura, escalaOrigem, escalaDesejada) {
-    let apoio;
-    if (escalaOrigem === "celsius") {
-        if(escalaDesejada === "kelvin") {
-            return temperatura + 273.15;
-        } else {
-            return (temperatura * 1.8) + 32;
-        }
-
-    } else if (escalaOrigem === "kelvin") {
-        apoio = temperatura - 273.15;
-        if(escalaDesejada === "celsius") {
-            return apoio;
-        } else {
-            return (apoio * 1.8) + 32;
-        }
-    } else {
-        apoio = (temperatura - 32) / 1.8;
-        if(escalaDesejada === "celsius") {
-            return apoio;
-        } else {
-            return apoio + 273.15;;
-        }
-    }
-}
-
-console.log(converteTemperatura(2.3, 'celsius', 'fahrenheit'));
